d76615f74e7f850c5f52b9aaf1a070ba61bdea65
soroush.fathi <soroush8fathi@gmail.com>
2021-04-05 03:35:36 +0430

    add inital classes

---
+++ src/sample/Players.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Players {
+    private String name;
+    private int id;
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
