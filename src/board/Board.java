7fc74d01b8d08e6010a3bdc8d29ae6302bfea42f
Eren Yeager <ErenYeager80@gmail.com>
2021-04-05 23:16:16 +0430

    change drag algorithm and add safe margin

--- src/board/Board.java
+++ src/board/Board.java
@@ -12,26 +12,33 @@ public class Board {
     private Group tileGroup = new Group();
     private Group pieceGroup = new Group();
     private Group wallGroup = new Group();
-    public static Group starGroup = new Group();
+    private Group starGroup = new Group();
+    private Group safeGroup = new Group();
 
     public static Tile[][] board;
+    public static Safe[][] safeMargin;
 
     public Board() {
             TILE_SIZE=70;
             WIDTH=10;
             HEIGHT=10;
             board=new Tile[WIDTH][HEIGHT];
+            safeMargin=new Safe[WIDTH][HEIGHT];
     }
     public Pane build(){
         Pane root = new Pane();
         root.setPrefSize(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
-        root.getChildren().addAll(tileGroup,wallGroup,starGroup,pieceGroup);
+        root.getChildren().addAll(tileGroup,wallGroup,starGroup,pieceGroup,safeGroup);
         Random rand= new Random();
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
                 Tile tile = new Tile((x + y) % 2 == 0, x, y);
                 board[x][y] = tile;
 
+                Safe safe=new Safe(x,y);
+                safeMargin[x][y] = safe;
+
+                safeGroup.getChildren().add(safe);
                 tileGroup.getChildren().add(tile);
 
                 Piece piece = null;
--- src/board/elements/Piece.java
+++ src/board/elements/Piece.java
@@ -3,9 +3,16 @@ package board.elements;
 import board.Board;
 import board.Element;
 import board.ElementType;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
+
+import java.awt.dnd.DropTargetDropEvent;
+import java.util.Map;
 
 import static board.Config.*;
 
@@ -14,24 +21,26 @@ public class Piece extends StackPane {
 
     private PieceType type;
 
-    private double mouseX, mouseY;
-    private double oldX, oldY;
+
+    private int x,y;
+
 
     public PieceType getType() {
         return type;
     }
 
     public double getOldX() {
-        return oldX;
+        return x;
     }
 
     public double getOldY() {
-        return oldY;
+        return y;
     }
 
     public Piece(PieceType type, int x, int y) {
         this.type = type;
-
+        this.x=x;
+        this.y=y;
         move(x, y);
 
         Ellipse bg = new Ellipse(TILE_SIZE * 0.3125, TILE_SIZE * 0.26);
@@ -55,40 +64,38 @@ public class Piece extends StackPane {
 
         getChildren().addAll(bg, ellipse);
 
-        setOnMouseDragged(e -> {
-            var selectedX = e.getSceneX() - TILE_SIZE / 2;
-            var selectedY = e.getSceneY() - TILE_SIZE / 2;
-            int _x = (int) Math.round(selectedX / TILE_SIZE);
-            int _y = (int) Math.round(selectedY / TILE_SIZE);
-            if (_x >= 0 && _y >= 0 && _x < WIDTH && _y < HEIGHT &&
-                    ( !Board.board[_x][_y].hasElement()||
-                    (Board.board[_x][_y].getElement().getType()!= ElementType.PIECE
-                    && Board.board[_x][_y].getElement().getType()!= ElementType.WALL))) {
-
-                relocate(_x * TILE_SIZE, _y * TILE_SIZE);
+     setOnDragDetected(e->{
+         for(int i=0;i<WIDTH ;i++)
+             if( !Board.board[i][this.y].hasElement()||
+                     (Board.board[i][this.y].getElement().getType()==ElementType.SLOW || Board.board[i][this.y].getElement().getType()==ElementType.STAR))
+                 Board.safeMargin[i][this.y].setVisible(true);
 
 
-                if(Board.board[_x][_y].hasElement() &&Board.board[_x][_y].getElement().getType()== ElementType.STAR){
-                    Board.board[_x][_y].getElement().star.move(10000,10000);
-                }
-                Board.board[_x][_y].setElement(new Element(this));
-                Board.board[(int) oldX / TILE_SIZE][(int) oldY / TILE_SIZE].setElement(null);
-                oldX = _x * TILE_SIZE;
-                oldY = _y * TILE_SIZE;
-            }
+         for(int j=0;j<HEIGHT;j++)
+             if( !Board.board[this.x][j].hasElement()||
+                     (Board.board[this.x][j].getElement().getType()==ElementType.SLOW || Board.board[this.x][j].getElement().getType()==ElementType.STAR))
+                 Board.safeMargin[this.x][j].setVisible(true);
 
-        });
+         Dragboard db = startDragAndDrop(TransferMode.ANY);
+         ClipboardContent cb = new ClipboardContent();
+         cb.putString(this.x+"|"+this.y);
+         db.setContent(cb);
+         e.consume();
+     });
+    setOnDragDone(e->{
+        for(int i =0;i<WIDTH;i++)
+            Board.safeMargin[i][this.y].setVisible(false);
 
-
+        for(int j=0;j<HEIGHT;j++)
+            Board.safeMargin[this.x][j].setVisible(false);
+    });
     }
 
     public void move(int x, int y) {
-        oldX = x * TILE_SIZE;
-        oldY = y * TILE_SIZE;
-        relocate(oldX, oldY);
+        this.x=x;
+        this.y=y;
+        relocate(x* TILE_SIZE, y* TILE_SIZE);
     }
 
-    public void abortMove() {
-        relocate(oldX, oldY);
-    }
+
 }
